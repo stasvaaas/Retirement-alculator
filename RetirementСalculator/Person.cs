083fc8b7accb9c2f9936acf631a9801c1d6c87a7
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-10-28 19:35:54 +0300

    Add project files.

---
+++ RetirementСalculator/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetirementСalculator
+{
+    internal class Person
+    {
+        public string Name { get; }
+        public int Age { get; }
+        public int YearsOfExperince { get; }
+        
+        public Person(string name, int age, int experiance)
+        {
+            Name = name;
+            Age = age;
+            YearsOfExperince = experiance;
+        }
+        public double GetRetiremetnValue(Func<int,int,double> values)
+        {
+            return values(Age, YearsOfExperince);
+        }
+    }
+}
---
+++ RetirementСalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RetirementСalculator
+{
+    internal class Program
+    {
+        public static double CalculatePension_Ukraine(int age, int experience)
+        {
+            return 0.2 * age + 0.1 * experience;
+        }
+
+        public static double CalculatePension_USA(int age, int experience)
+        {
+            return 0.3 * age + 0.2 * experience;
+        }
+
+        public static double CalculatePension_Japan(int age, int experience)
+        {
+            return 0.4 * age + 0.3 * experience;
+        }
+
+        public static void Main()
+        {
+            Person person = new Person("John", 60, 40);
+            double pension_Ukraine = person.GetRetiremetnValue(CalculatePension_Ukraine);
+            double pension_USA = person.GetRetiremetnValue(CalculatePension_USA);
+            double pension_Japan = person.GetRetiremetnValue(CalculatePension_Japan);
+        }
+    }
+}
